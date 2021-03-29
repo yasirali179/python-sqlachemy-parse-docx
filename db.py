71ff6f5b4518ded8d85af84f9961bd9c0fe8f00a
yasirali5144 <yasir.ali5144@gmail.com>
2021-03-29 16:27:23 +0500

    add db and add confidential metters

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.sql.sqltypes import ARRAY
 from sqlalchemy.orm import relationship
 meta = MetaData()
+engine = create_engine('sqlite:///data.db', echo=True)
 
 
 Data = Table(
@@ -10,77 +11,179 @@ Data = Table(
     Column('publication_id', String),
     Column('subsection_id', String),
     Column('submissions_id', String),
-    # relationship("PreliminaryInformation", "PreliminaryInformation"),
-    # relationship("DepartmentInformation", "DepartmentInformation"),
-    # relationship("Feedback", "Feedback"),
-    # relationship("PublishableInformation", "PublishableInformation"),
-    # relationship("ConfidentialInformation", "ConfidentialInformation")
+    Column('preliminary_information_id', Integer,
+           ForeignKey('PreliminaryInformation.id')),
+    Column('department_information_id', Integer,
+           ForeignKey('DepartmentInformation.id')),
+    Column('feedback_id', Integer, ForeignKey('Feedback.id')),
+    Column('publishable_information_id', Integer,
+           ForeignKey('PublishableInformation.id')),
+    Column('confidential_information_id', Integer,
+           ForeignKey('ConfidentialInformation.id')),
 )
 
+# ------------------------------ Preliminary Information ------------------------------
 PreliminaryInformation = Table(
     'PreliminaryInformation', meta,
     Column('id', Integer, primary_key=True),
-    Column('data_id', Integer, ForeignKey('Data.id')),
     Column('firm_name', String),
     Column('practice_area', String),
     Column('location_jurisdiction', String),
-    #relationship("ContactPersonArrangeInterviews", "ContactPersonArrangeInterviews")
+    Column('contact_person_arrange_interviews_id', Integer,
+           ForeignKey('ContactPersonArrangeInterviews.id')),
 )
 
+ContactPersonArrangeInterviews = Table(
+    'ContactPersonArrangeInterviews', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('email', String),
+    Column('phone', String),
+)
+
+# ------------------------------ Department Information ------------------------------
 DepartmentInformation = Table(
     'DepartmentInformation', meta,
     Column('id', Integer, primary_key=True),
-    Column('data_id', Integer, ForeignKey('Data.id')),
     Column('department_name', String),
     Column('partners', Integer),
     Column('qualified_lawyers', Integer),
     Column('male_ratio', Integer),
     Column('female_ratio', Integer),
     Column('department_best_known_for', String),
-    # relationship("HeadsofDepartment", "HeadsofDepartment"),
-    # relationship("Hires", "Hires"),
-    # relationship("RankedLawyersInformation", "RankedLawyersInformation"),
-    # relationship("UnrankedLawyersInformation", "UnrankedLawyersInformation"),
+    Column('heads_of_department_id', Integer,
+           ForeignKey('HeadsofDepartment.id')),
+    Column('hires_id', Integer,
+           ForeignKey('Hires.id')),
+    Column('ranked_lawyers_information_id', Integer,
+           ForeignKey('RankedLawyersInformation.id')),
+    Column('unranked_lawyers_information_id', Integer,
+           ForeignKey('UnrankedLawyersInformation.id')),
 )
 
-Feedback = Table(
-    'Feedback', meta,
+HeadsofDepartment = Table(
+    'HeadsofDepartment', meta,
     Column('id', Integer, primary_key=True),
-    Column('data_id', Integer, ForeignKey('Data.id')),
-    Column('provious_coverage_feedback', String),
-    # relationship("BarristersAdvocatesInfo", "BarristersAdvocatesInfo"),
-)
-
-PublishableInformation = Table(
-    'PublishableInformation', meta,
-    Column('id', Integer, primary_key=True),
-    Column('data_id', Integer, ForeignKey('Data.id')),
-    # relationship("PublishableClients", "PublishableClients"),
-    # relationship("PublishableMatters", "PublishableMatters"),
-)
-
-ConfidentialInformation = Table(
-    'ConfidentialInformation', meta,
-    Column('id', Integer, primary_key=True),
-    Column('data_id', Integer, ForeignKey('Data.id')),
-    # relationship("ConfidentialMatters", "ConfidentialMatters"),
-)
-
-
-
-
-ContactPersonArrangeInterviews = Table(
-    'ContactPersonArrangeInterviews', meta,
-    Column('id', Integer, primary_key=True),
-    Column('PreliminaryInformation', Integer, ForeignKey('PreliminaryInformation.id')),
-
     Column('name', String),
     Column('email', String),
     Column('phone', String),
 )
+Hires = Table(
+    'Hires', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('joined', String),
+    Column('joined_from', String),
+)
+RankedLawyersInformation = Table(
+    'RankedLawyersInformation', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('comment', String),
+    Column('partner', String),
+)
+UnrankedLawyersInformation = Table(
+    'UnrankedLawyersInformation', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('comment', String),
+    Column('partner', String),
+)
 
+# ------------------------------ Feedback ------------------------------
+Feedback = Table(
+    'Feedback', meta,
+    Column('id', Integer, primary_key=True),
+    Column('provious_coverage_feedback', String),
+    Column('barristers_advocates_info_id', Integer,
+           ForeignKey('BarristersAdvocatesInfo.id')),
+)
+
+BarristersAdvocatesInfo = Table(
+    'BarristersAdvocatesInfo', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('firm', String),
+    Column('comments', String),
+)
+
+# ------------------------------ Publishable Information ------------------------------
+PublishableInformation = Table(
+    'PublishableInformation', meta,
+    Column('id', Integer, primary_key=True),
+    Column('publishable_clients_id', Integer,
+           ForeignKey('PublishableClients.id')),
+)
+
+PublishableClients = Table(
+    'PublishableClients', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('new_lient ', String),
+)
+
+PublishableMatters = Table(
+    'PublishableMatters', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('summary', String),
+    Column('value', String),
+    Column('cross_border', String),
+    Column('lead_partner', String),
+    Column('other_team_members', String),
+    Column('other_firms_advising', String),
+    Column('date_of_completion', String),
+    Column('other_information', String),
+    Column('publishable_information_id', Integer,
+           ForeignKey('PublishableInformation.id')),
+)
+
+# ------------------------------ Confidential Information ------------------------------
+
+ConfidentialInformation = Table(
+    'ConfidentialInformation', meta,
+    Column('id', Integer, primary_key=True),
+    Column('confidential_clients_id', Integer,
+           ForeignKey('ConfidentialClients.id')),
+)
+
+ConfidentialClients = Table(
+    'ConfidentialClients', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('new_lient ', String),
+)
+
+ConfidentialMatters = Table(
+    'ConfidentialMatters', meta,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('summary', String),
+    Column('value', String),
+    Column('cross_border', String),
+    Column('lead_partner', String),
+    Column('other_team_members', String),
+    Column('other_firms_advising', String),
+    Column('date_of_completion', String),
+    Column('other_information', String),
+    Column('confidential_information_id', Integer,
+           ForeignKey('ConfidentialInformation.id')),
+)
+
+
+def insert_Data(publication_id, subsection_id, submissions_id):
+    ins = Data.insert().values(publication_id=publication_id,
+                               subsection_id=subsection_id, submissions_id=submissions_id)
+    conn = engine.connect()
+    result = conn.execute(ins)
+
+
+def get_Data():
+    s = Data.select()
+    conn = engine.connect()
+    result = conn.execute(s)
+    return result
 
 
 def create():
-    engine = create_engine('sqlite:///data.db', echo=True)
     meta.create_all(engine)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from db import create
+from db import create, get_Data, insert_Data
 from docx2python import docx2python
 
 
 def parsing(result):
     i = 0
-    import pdb
-    pdb.set_trace()
+    # import pdb
+    # pdb.set_trace()
     while i < len(result.body):
         if len(result.body[i]) >= 2:
             data = result.body[i]
@@ -13,6 +13,7 @@ def parsing(result):
             get_department_information(data)
             get_feedback(data)
             get_publishable_information(data)
+            get_confidential_information(data)
 
         i = i+1
 
@@ -210,6 +211,60 @@ def get_publishable_information(data):
             row = row+1
 
 
+def get_confidential_information(data):
+    if len(data[0]) == 1 and len(data[0][0]) == 1 and "List of this department's CONFIDENTIAL clients" in data[0][0][0]:
+        print("-------------------------- Confidential Information----------------------")
+        print("List of this department's CONFIDENTIAL clients")
+        row = 2
+        while row < len(data):
+            row_data = data[row]
+            if len(get_text(row_data[0])) > 0 and "N/A" != get_text(row_data[0]):
+                print("Client " + str(row-1) + " Details")
+                col = 1
+                while col < len(row_data):
+                    if col == 1:
+                        print("Name of Client: " + get_text(row_data[col]))
+                    elif col == 2:
+                        print("New Client (Yes/No): " +
+                              get_text(row_data[col]))
+                    col = col+1
+            row = row+1
+    elif len(data[0]) >= 1 and len(data[0][0]) >= 1 and (("Confidential Work Highlights" in data[0][0][0] and "Publishable Matter" in data[1][0][0]) or ("Confidential Matter" in data[0][0][0])):
+        print(get_text(data[0][0]))
+        row = 2
+        # import pdb; pdb.set_trace();
+        while row < len(data):
+            row_data = data[row]
+            if len(get_text(row_data[0])) > 0 and "N/A" not in get_text(row_data[0]):
+                if "Name of client" in get_text(row_data[0]):
+                    print("Name of client: " + get_text(data[row+1][0]))
+                elif "Summary of matter and your department" in get_text(row_data[0]):
+                    print("Summary of matter and your department: " +
+                          get_text(data[row+1][0]))
+                elif "Summary of matter and your department" in get_text(row_data[0]):
+                    print("Summary of matter and your department: " +
+                          get_text(data[row+1][0]))
+                elif "Matter value" in get_text(row_data[0]):
+                    print("Matter value: " + get_text(data[row+1][0]))
+                elif "Is this a cross-border matter" in get_text(row_data[0]):
+                    print("Is this a cross-border matter: " +
+                          get_text(data[row+1][0]))
+                elif "Lead partner" in get_text(row_data[0]):
+                    print("Lead partner: " + get_text(data[row+1][0]))
+                elif "Other team members" in get_text(row_data[0]):
+                    print("Other team members: " + get_text(data[row+1][0]))
+                elif "Other firms advising on the matter" in get_text(row_data[0]):
+                    print("Other firms advising on the matter: " +
+                          get_text(data[row+1][0]))
+                elif "Date of completion or current status" in get_text(row_data[0]):
+                    print("Date of completion or current status: " +
+                          get_text(data[row+1][0]))
+                elif "Other information about this matter" in get_text(row_data[0]):
+                    print("Other information about this matter: " +
+                          get_text(data[row+1][0]))
+            row = row+1
+
+
 def get_text(data):
     index = 0
     final_text = ''
@@ -222,3 +277,9 @@ def get_text(data):
 if __name__ == "__main__":
     result = docx2python('3362805.docx')
     parsing(result)
+    create()
+    # insert_Data("a", 's', 'f')
+    # result = get_Data()
+
+    # for row in result:
+    #     print(row)
